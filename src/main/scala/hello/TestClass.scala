11c1b95eb0f5e25c3efeb008636b032aee6fae60
Anjaneya Jha <anjaneya4@gmail.com>
2018-09-11 00:14:54 +0530

    Add new files

--- src/main/scala/hello/TestClass.scala
+++
@@ -1,9 +0,0 @@
-package hello
-
-class TestClass {
-
-}
-
-object TestClass extends App{
-  println("Hello from Scala + Gradle!!!")
-}
--- src/main/scala/server/MyWebController.scala
+++ src/main/scala/server/MyWebController.scala
@@ -6,13 +6,18 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation._
 
 @Controller
-@RequestMapping(Array("/puzzles"))
+@RequestMapping(Array("/puzzles", "/"))
 class MyWebController @Autowired()(){
 
   @GetMapping
   def list(model: Model) = {
     model.addAttribute("username", "Anjaneya")
     "puzzles/mypuzzles"
+  }
+
+  @GetMapping(Array("/puzzlme"))
+  def puzzleme(model: Model) = {
+    "puzzles/puzzlme"
   }
 
 }
